dda13a076689f17f2c331ad7c625f9f2ba881d65
JesseXC <100256437+JesseXC@users.noreply.github.com>
2023-06-23 12:25:45 -0700

    Update youtubeApi.py

--- youtubeApi.py
+++ youtubeApi.py
@@ -2,7 +2,7 @@ import requests
 from googleapiclient.discovery import build
 
 base_url = "https://www.googleapis.com/youtube/v3"
-api_key = 'AIzaSyAUTGuVJmt1eCA33Se8Nvu1Pl8_KYi8RdU'
+api_key = ''
 headers = {
     "Content-Type": "application/json",
 }
