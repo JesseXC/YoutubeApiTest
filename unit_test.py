c67646cc832f4d1cb880201c2efb4520b718275d
Jesse Cruz <jcruz2@oberlin.edu>
2023-06-23 20:32:47 +0000

    fixed up youtubeApi, now contains 3 functions, added subscriber battle, class object can now house multiple channels and stats. Yaml now conducts unit test, added unit test.py

---
+++ unit_test.py
@@ -0,0 +1,25 @@
+import unittest
+from googleapiclient.discovery import build
+from youtubeApi import ChannelStats
+
+api_key = ''
+channels = ["CNN", "FoxNews"]
+
+class ChannelStatsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel_stats = ChannelStats(api_key, channels)
+
+    def test_get_channel_statistics(self):
+        channel_statistics = self.channel_stats.get_channel_statistics()
+        self.assertEqual(len(channel_statistics), len(channels))
+  
+    def test_add_channel(self):
+        initial_channel_count = len(self.channel_stats.channels)
+        self.channel_stats.add_channel("BBC")
+        new_channel_count = len(self.channel_stats.channels)
+        self.assertEqual(new_channel_count, initial_channel_count + 1)  # Assuming one channel was added
+
+if __name__ == '__main__':
+    unittest.main()
--- youtubeApi.py
+++ youtubeApi.py
@@ -1,19 +1,19 @@
 import requests
 from googleapiclient.discovery import build
 
-base_url = "https://www.googleapis.com/youtube/v3"
-api_key = ''
-headers = {
-    "Content-Type": "application/json",
-}
-url = f"{base_url}/videos"
-params = {
-    "part": "snippet",
-    "id": 'iUMgIPlWHO8',
-    "key": api_key
-}
-response = requests.get(url, headers=headers, params=params)
-response_json = response.json()
+# base_url = "https://www.googleapis.com/youtube/v3"
+api_key = 'AIzaSyAUTGuVJmt1eCA33Se8Nvu1Pl8_KYi8RdU'
+# headers = {
+#     "Content-Type": "application/json",
+# }
+# url = f"{base_url}/videos"
+# params = {
+#     "part": "snippet",
+#     "id": 'iUMgIPlWHO8',
+#     "key": api_key
+# }
+# response = requests.get(url, headers=headers, params=params)
+# response_json = response.json()
 
 youtube = build(
     'youtube',
@@ -21,28 +21,42 @@ youtube = build(
     developerKey=api_key
 )
 
-
-class YTstats:
-    def __init__(self, apikey, channel_id):
+class ChannelStats:
+    def __init__(self, apikey, channels=['Youtube']):
         self.api_key = apikey
-        self.channel_id = channel_id
-        self.channel_statistics = None
+        self.channels = [channel for channel in channels]
+        self.channel_statistics = []
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def get_channel_statistics(self):
-        request = youtube.channels().list(
-            part='statistics',
-            forUsername=f'{self.channel_id}'
-        )
-        response = request.execute()
-        self.channel_statistics = response['items'][0]['statistics']
+        for channel in self.channels:
+            request = self.youtube.channels().list(
+                part='statistics',
+                forUsername=f'{channel}'
+            )
+            response = request.execute()
+            self.channel_statistics.append(response['items'][0]['statistics'])
         return self.channel_statistics
 
-    def get_subscribers(self):
-        return self.channel_statistics['subscriberCount']
+    def get_subscribers(self, channel_name):
+        return int(self.channel_statistics[self.channels.index(channel_name)]['subscriberCount'])
+
+    def battle_subcriberCount(self):
+        if len(self.channels) == 1:
+            return self.get_subscribers(self.channels[0])
+        max_info = [self.get_subscribers(self.channels[0]), self.channels[0]]
+        for i in range(1, len(self.channels)):
+            if self.get_subscribers(self.channels[i]) > max_info[0]:
+                max_info = [self.get_subscribers(self.channels[i]), self.channels[i]]
+        print(f"The Winner of the Subscriber Battle is: {max_info[1]} with {max_info[0]} subscribers!")
+        return max_info
+
+    def add_channel(self, channel_name):
+        self.channels.append(channel_name)
 
 
-CNN_Channel = YTstats(api_key, "CNN")
+CNN_Fox_Info = ChannelStats(api_key, ["CNN", "FoxNews"])
 
-print(CNN_Channel.get_channel_statistics())
-print(CNN_Channel.get_subscribers())
+print(CNN_Fox_Info.get_channel_statistics())
+print(CNN_Fox_Info.get_subscribers("CNN"))
+max_subscribed = CNN_Fox_Info.battle_subcriberCount()
