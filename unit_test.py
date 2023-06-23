d5ec0934888caba8e0e3f59846f7ba2a2f7a6b55
JesseXC <100256437+JesseXC@users.noreply.github.com>
2023-06-23 13:43:15 -0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -1,9 +1,11 @@
 import unittest
 from googleapiclient.discovery import build
+
 from youtubeApi import ChannelStats
 
 api_key = ''
 channels = ["CNN", "FoxNews"]
+
 
 class ChannelStatsTest(unittest.TestCase):
 
@@ -14,12 +16,13 @@ class ChannelStatsTest(unittest.TestCase):
     def test_get_channel_statistics(self):
         channel_statistics = self.channel_stats.get_channel_statistics()
         self.assertEqual(len(channel_statistics), len(channels))
-  
+
     def test_add_channel(self):
         initial_channel_count = len(self.channel_stats.channels)
         self.channel_stats.add_channel("BBC")
         new_channel_count = len(self.channel_stats.channels)
         self.assertEqual(new_channel_count, initial_channel_count + 1)  # Assuming one channel was added
+
 
 if __name__ == '__main__':
     unittest.main()
